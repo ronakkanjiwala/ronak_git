803f87cbf5a33ee8a6dc59744b5279a0f9f40a7b
ronakkanjiwala <112809883+ronakkanjiwala@users.noreply.github.com>
2022-09-04 18:23:15 +0530

    Final data for the test

--- Employee_data.pl
+++ Employee_data.pl
@@ -1,11 +1,9 @@
-
 #!/usr/bin/perl
 use Data::Dumper;
 use DBI;
 use DateTime;
 
 my $datetime = DateTime->now;
-
 
 my $driver = "mysql";
 my $database = 'emplpyee';
@@ -15,20 +13,20 @@ my $password = 'Dha-Roy@18';
 
 my $dbh = DBI->connect($dsn, $userid, $password) or die $DBI::errstr;
 
+my $file = 'C:\Users\ronny\ronak.csv'; 
 
-my $file = 'C:\Users\ronny\ronak.csv';
-print Dumper $file; 
-my $sum = 0;
 open(my $data, '<', $file) or die "Could not open '$file' $!\n";
+
 while (my $line = <$data>) {
- chomp($line);
-  my (@rd) = split( "," , $line);
-  $sum += $fields[2];
-  print Dumper $rd[2];
-  my $sth = $dbh->prepare("insert into employee (firstname, lastname, age , created_on, comments)
+    chomp($line);
+    my (@empoyee_data) = split( "," , $line);    
+    eval{
+        my $sth = $dbh->prepare("insert into employee (firstname, lastname, age , created_on, comments)
                         values
-                       ('$rd[0]', '$rd[1]' ,$rd[2],'$datetime',$rd[3])");
-$sth->execute() or die $DBI::errstr;
-
-
+                       ('$empoyee_data[0]', '$empoyee_data[1]' ,$empoyee_data[2],'$datetime',$empoyee_data[3])");
+        $sth->execute() or die $DBI::errstr;
+    };
+    if($@){
+       print $@;
+    }
 }
